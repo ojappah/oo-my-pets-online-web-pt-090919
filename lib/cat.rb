8f455e32bcd6994dd3e1c173305dfdd8d919b574
generous-vector-9581 <ojappah@gmail.com>
2019-10-16 18:49:21 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
-class Cat
-  # code goes here
-    attr_reader :name
-  attr_accessor :mood
 
-  def initialize(name, mood = "nervous")
-    @mood = mood
+class Cat	
+  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
     @name = name
+    @mood = "nervous"
   end
 end
